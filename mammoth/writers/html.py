c2648aa6a9e970751802cdb06e4be423ee19ceb7
Akkana Peck <akkana@shallowsky.com>
2015-06-09 12:48:15 -0600

    Add line breaks around paragraph and list tags

--- mammoth/writers/html.py
+++ mammoth/writers/html.py
@@ -6,6 +6,8 @@ import cgi
 class HtmlWriter(object):
     def __init__(self):
         self._fragments = []
+        self.break_after_start = [ 'p', 'ul', 'ol' ];
+        self.break_after_end = [ 'p', 'ul', 'ol', 'li' ];
     
     def text(self, text):
         self._fragments.append(_escape_html(text))
@@ -13,12 +15,12 @@ class HtmlWriter(object):
     def start(self, name, attributes=None):
         attribute_string = _generate_attribute_string(attributes)
         self._fragments.append("<{0}{1}>".format(name, attribute_string))
-        if name == 'p':
+        if name in self.break_after_start:
             self._fragments.append("\n")
 
     def end(self, name):
         self._fragments.append("</{0}>".format(name))
-        if name == 'p':
+        if name in self.break_after_end:
             self._fragments.append("\n")
     
     def self_closing(self, name, attributes=None):
